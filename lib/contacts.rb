4a91ad0896392ee57b23054102bf54160c7aa690
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-24 14:04:41 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,19 @@ require 'pry'
   #   }
   # }
 
+  contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
+
 def remove_strawberry(contacts)
    contacts["Freddy Mercury"][favorite_ice_cream_flavors].each do |key, value|
      if value == "strawberry"
